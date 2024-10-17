b6be89b5b745c8f5e2de031619e13d6ad66699b2
zzl <zhaoaa966@163.com>
2024-10-17 21:08:37 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,33 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
+  theme = "everforest",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+    -- project
+    { txt = "  Projects", keys = "pp", cmd = "Telescope projects" },
+
+    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+  },
 }
 
 return M
